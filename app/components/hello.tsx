4bdab444129f94faf95a6f80bcdf0ea6b006e54e
htikelinn <htikezawlinn@gmail.com>
2025-09-15 11:12:12 +0630

    update and add some code

---
+++ app/components/hello.tsx
@@ -0,0 +1,7 @@
+'use client'
+function Hello() {
+    console.log("What am I doing here? CLIENT-SIDE RENDERING");
+    return <h1 className="text-5xl text-emerald-600">Client Side Rendering Test</h1>;
+}
+
+export default Hello;
