3749809f31d54b53f6445c9a71ea06270b419d60
htikelinn <htikezawlinn@gmail.com>
2025-09-15 11:26:44 +0630

    add count function

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,23 @@
+'use client'
+import { useState } from "react";
 import Hello from "./components/hello";
 
 export default function Home() {
     console.log("What am I doing here? SERVER-SIDE RENDERING");
+    const [count, setCount] = useState(0);
+
     return (
         <>
-            <h1 className="text-5xl text-pink-600">Welcome to Next.js</h1>
+            <h1 className="text-5xl text-pink-600 text-center">Welcome to Next.js</h1>
             <h2 className="text-3xl text-sky-600">Htin Project Deploy to VERCEL Cloud</h2>
             <p>{new Date().toLocaleTimeString()}</p>
+            <p className="text-2xl">This is Testing Page to Deploy form Git to vercel Cloud</p>
+            <div className="flex gap-2">
+                <h1>Count: {count}</h1>
+                <button onClick={() => setCount(count + 1)}>Count++</button>
+                <button onClick={() => setCount(count - 1)}>Count--</button>
+                <button onClick={() => setCount(0)}>Reset</button>
+            </div>
 
             <Hello />
         </>
