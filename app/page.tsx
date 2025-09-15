fc9f316e85fe8811c964c97433bf8180472d7b6e
htikelinn <htikezawlinn@gmail.com>
2025-09-15 11:30:59 +0630

    update code page and hello tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,20 +3,19 @@ import { useState } from "react";
 import Hello from "./components/hello";
 
 export default function Home() {
-    console.log("What am I doing here? SERVER-SIDE RENDERING");
     const [count, setCount] = useState(0);
 
     return (
         <>
             <h1 className="text-5xl text-pink-600 text-center">Welcome to Next.js</h1>
-            <h2 className="text-3xl text-sky-600">Htin Project Deploy to VERCEL Cloud</h2>
+            <h2 className="text-3xl text-sky-600 text-center">Htin Project Deploy to VERCEL Cloud</h2>
             <p>{new Date().toLocaleTimeString()}</p>
             <p className="text-2xl">This is Testing Page to Deploy form Git to vercel Cloud</p>
-            <div className="flex gap-2">
+            <div className="flex gap-3">
                 <h1>Count: {count}</h1>
-                <button onClick={() => setCount(count + 1)}>Count++</button>
-                <button onClick={() => setCount(count - 1)}>Count--</button>
-                <button onClick={() => setCount(0)}>Reset</button>
+                <button className="btn btn-primary px-2" onClick={() => setCount(count + 1)}>Increase</button>
+                <button className="btn btn-warning px-2" onClick={() => setCount(count - 1)}>Decrease</button>
+                <button className="btn btn-error px-2" onClick={() => setCount(0)}>Reset</button>
             </div>
 
             <Hello />
