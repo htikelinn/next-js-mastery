008527c248e84dac95af6aad2321cc35c3685752
htikelinn <htikezawlinn@gmail.com>
2025-09-15 11:20:16 +0630

    add 2 line in app/page

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,9 @@ export default function Home() {
     return (
         <>
             <h1 className="text-5xl text-pink-600">Welcome to Next.js</h1>
+            <h2 className="text-3xl text-sky-600">Htin Project Deploy to VERCEL Cloud</h2>
+            <p>{new Date().toLocaleTimeString()}</p>
+
             <Hello />
         </>
     );
